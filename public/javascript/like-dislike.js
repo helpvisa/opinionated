daef28121bf63307bec8e2eaa750af8f15579dd3
dan brackenbury <daniel.brackenbury@gmail.com>
2022-10-13 14:01:40 -0400

    added like/dislike handling, posting new opinion

---
+++ public/javascript/like-dislike.js
@@ -0,0 +1,41 @@
+// handles likes and dislikes
+// listen for clicks on child elements
+opinionEl = document.querySelector('#opinion-container');
+opinionEl.addEventListener('click', (event) => {
+    if (event.target.classList.contains("like")) {
+        id = event.target.getAttribute("data-post-id");
+        // console.log('liked' + id);
+        sendLike(id, event.target);
+    } else if (event.target.classList.contains("dislike")) {
+        id = event.target.getAttribute("data-post-id");
+        // console.log('disliked' + id);
+        sendDislike(id, event.target);
+    } else if (event.target.classList.contains("comment-page")) {
+        id = event.target.getAttribute("data-post-id");
+        // console.log('go to comments' + id);
+        document.location.replace('/posts/' + id);
+    }
+});
+
+// functions
+async function sendLike(id, el) {
+    const response = await fetch('/api/posts/like/' + id, {
+        method: 'put'
+    });
+
+    // check response
+    if (response.ok) {
+        document.location.reload();
+    }
+}
+
+async function sendDislike(id, el) {
+    const response = await fetch('/api/posts/dislike/' + id, {
+        method: 'put'
+    });
+
+    // check response
+    if (response.ok) {
+        document.location.reload();
+    }
+}
